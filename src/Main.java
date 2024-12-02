26e9262145689013a824750fba7650d2babcf1e6
Puttzer <carragher@live.se>
2024-12-02 19:11:04 +0100

    Double

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int firstNumber;
+
+        // This is the calc with Integers.
+
+      /*  int firstNumber;
         int secondNumber;
         // Scenario, you want to add one number with another number and see the total.
         System.out.println("Please input the 2 integers you wish to add together.");
@@ -20,7 +23,22 @@ public class Main {
 
         // this line does simple math and outputs the total.
         System.out.println("The total for " + firstNumber + " + " + secondNumber + " is: " + (firstNumber + secondNumber));
-// main
+
+*/
+        double firstNumber;
+        double secondNumber;
+        // Scenario, you want to add one number with another number and see the total.
+        System.out.println("Please input the 2 values you wish to calculate.");
+
+
+        // this line enables the user to input an integer and stores it.
+        firstNumber = Double.parseDouble(scanner.nextLine());
+        System.out.println(" Input second value ");
+        // this line enables the user to input an integer and stores it.
+        secondNumber = Double.parseDouble(scanner.nextLine());
+
+        // this line does simple math and outputs the total.
+        System.out.println("The total for " + firstNumber + " + " + secondNumber + " is: " + (firstNumber + secondNumber));
 
     }
 }
